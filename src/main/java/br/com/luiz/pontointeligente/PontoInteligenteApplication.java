a2ffb6f9b2a75a0efcfc52e53770cec37f6e9d57
Luiz Pinheiro <luizpinheiro.rs@gmail.com>
2019-04-07 13:34:03 -0300

    Adicionado controller CadastroPJ

--- src/main/java/br/com/luiz/pontointeligente/PontoInteligenteApplication.java
+++ src/main/java/br/com/luiz/pontointeligente/PontoInteligenteApplication.java
@@ -2,12 +2,17 @@ package br.com.luiz.pontointeligente;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
+
 
 }
--- src/main/java/br/com/luiz/pontointeligente/entities/Empresa.java
+++ src/main/java/br/com/luiz/pontointeligente/entities/Empresa.java
@@ -21,7 +21,7 @@ public class Empresa implements Serializable {
     }
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long getId() {
         return id;
     }
